fa38fb86a19ce4815bd4e07212c4d4731f73e544
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-04-25 18:21:49 +0200

    start exercise2

---
+++ Pattern Recognition/02/ex02_files/compute_qda.m
@@ -0,0 +1,17 @@
+function [mu0, mu1, covmat0, covmat1, p0, p1] = compute_qda(trainingy,trainingx)
+% computes means mu0 mu1 for both exams and their covariance matrices
+% covmat0 covmat1 well es priors p0 and p1
+% Input: labels (trainingy) and exam results (trainingx)
+
+
+indices0 = trainingy == 0;
+indices1 = ~indices0;
+
+mu0 = mean(trainingx(:, indices0), 2);
+mu1 = mean(trainingy(:, indices1), 2);
+
+covmat0 = cov((trainingx(:, indices0)'))
+covmat1 = cov((trainingx(:, indices1)'))
+
+p0 = 1;
+p1 = 1;
---
+++ Pattern Recognition/02/ex02_files/ex02.m
@@ -0,0 +1,5 @@
+clear; close all; clc;
+
+fprintf('loading data\n');
+load('student-scores.mat');
+
