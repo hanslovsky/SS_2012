f6faea71a9101c5e2b8c207e6b17f22599997cb8
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-07 19:04:38 +0200

    algos 6 treeInsert

--- Algos/06/treap.py
+++ Algos/06/treap.py
@@ -4,7 +4,7 @@
 import random
 
 class Node(object):
-    def __init__(self, key, priority):
+    def __init__(self, key, priority = 0):
         self.key = key
         self.priority = priority
         self.left = None
@@ -23,15 +23,36 @@ def treeRotateRight(rootnode):
     return newRootNode
 
 def randomTreapInsert(rootnode, key):
-    randomPriority = random.random()
     if rootnode is None:
+        randomPriority = random.random()
         return Node(key, randomPriority)
     if key == rootnode.key:
         return rootnode
     elif key < rootnode.key:
         rootnode.left = randomTreapInsert(rootnode.left, key)
+        if rootnode.left.priority > rootnode.priority:
+            rootnode = treeRotateRight(rootnode)
     else:
         rootnode.right = randomTreapInsert(rootnode.right, key)
+        if rootnode.right.priority > rootnode.priority:
+            rootnode = treeRotateLeft(rootnode)
+    return rootnode
+
+def dynamicTreapInsert(rootnode, key):
+    if rootnode is None:
+        priority = 1
+        return Node(key, priority)
+    if key == rootnode.key:
+        rootnode.priority += 1
+        return rootnode
+    elif key < rootnode.key:
+        rootnode.left = dynamicTreapInsert(rootnode.left, key)
+        if rootnode.left.priority > rootnode.priority:
+            rootnode = treeRotateRight(rootnode)
+    else:
+        rootnode.right = dynamicTreapInsert(rootnode.right, key)
+        if rootnode.right.priority > rootnode.priority:
+            rootnode = treeRotateLeft(rootnode)
     return rootnode
 
 def treeDepth(rootnode):
@@ -39,3 +60,26 @@ def treeDepth(rootnode):
         return 0;
     else:
         return max((treeDepth(rootnode.left), treeDepth(rootnode.right))) + 1
+
+def treePrint(rootnode):
+    if rootnode is not None:
+        print "Key:", rootnode.key
+        print "Priority:", rootnode.priority, "\n"
+        if rootnode.left is not None:
+            treePrint(rootnode.left)
+        if rootnode.right is not None:
+            treePrint(rootnode.right)
+
+
+if __name__ == "__main__":
+    a = [1, 2, 3, 4, 0, 3]
+    dynTree = None
+    ranTree = None
+
+    for el in a:
+        dynTree = dynamicTreapInsert(dynTree, el)
+        ranTree = randomTreapInsert(ranTree, el)
+
+    print a
+    treePrint(dynTree)
+    
