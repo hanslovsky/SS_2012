78cad2bba46b4632adab37a66bebfe0c1fd07175
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-07 20:53:26 +0200

    algos 6.1 done

--- Algos/06/treap.py
+++ Algos/06/treap.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+import numpy as np
 
 class Node(object):
     def __init__(self, key, priority = 0):
@@ -10,6 +11,7 @@ class Node(object):
         self.left = None
         self.right = None
 
+# rotations (1a)
 def treeRotateLeft(rootnode):
     newRootNode = rootnode.right
     rootnode.right = newRootNode.left
@@ -22,6 +24,7 @@ def treeRotateRight(rootnode):
     newRootNode.right = rootnode
     return newRootNode
 
+# insert (1b)
 def randomTreapInsert(rootnode, key):
     if rootnode is None:
         randomPriority = random.random()
@@ -55,11 +58,19 @@ def dynamicTreapInsert(rootnode, key):
             rootnode = treeRotateLeft(rootnode)
     return rootnode
 
+# helper functions
 def treeDepth(rootnode):
     if rootnode == None:
         return 0;
     else:
         return max((treeDepth(rootnode.left), treeDepth(rootnode.right))) + 1
+
+def treeSize(rootnode):
+    if rootnode == None:
+        return 0;
+    else:
+        return treeSize(rootnode.left) + treeSize(rootnode.right) + 1
+    
 
 def treePrint(rootnode):
     if rootnode is not None:
@@ -70,16 +81,84 @@ def treePrint(rootnode):
         if rootnode.right is not None:
             treePrint(rootnode.right)
 
+def treePrint2(rootnode):
+    if rootnode is not None:
+        treePrint2(rootnode.left)
+        print "Key:", rootnode.key
+        print "Priority:", rootnode.priority, "\n"
+        treePrint2(rootnode.right)
+
+# convert tree to arrays of keys, priorities and corresponding levels
+# sorted by keys (increasing order)
+# neccessarry to see if two trees contain the same elements and ordered the same
+# way with regard to keys
+def treeArr(rootnode, keys, priorities, levels, depth = 0):
+    if rootnode is not None:
+        treeArr(rootnode.left, keys, priorities, levels, depth + 1)
+        keys +=  [rootnode.key]
+        priorities += [rootnode.priority]
+        levels += [depth]
+        treeArr(rootnode.right, keys, priorities, levels, depth + 1)
+
+
 
 if __name__ == "__main__":
-    a = [1, 2, 3, 4, 0, 3]
-    dynTree = None
-    ranTree = None
 
-    for el in a:
-        dynTree = dynamicTreapInsert(dynTree, el)
-        ranTree = randomTreapInsert(ranTree, el)
 
-    print a
-    treePrint(dynTree)
-    
+
+    # create trees containing text from 'die-drei-musketiere.txt' (1c)
+    s = file('die-drei-musketiere.txt').read()
+    for k in ',;.:-"\'!?':
+        s = s.replace(k, '')
+    s=s.lower()
+    text = s.split()
+    randomTreap = None
+    dynamicTreap = None
+    for word in text:
+        randomTreap = randomTreapInsert(randomTreap, word)
+        dynamicTreap = dynamicTreapInsert(dynamicTreap, word)
+
+    # print total number of words and number of nodes (i.e. number of different words) for each tree
+    print "total words:", len(text), \
+          "\nnodes in random Treap:", treeSize(randomTreap), \
+          "\nnodes in dynamic Treap:", treeSize(dynamicTreap), "\n"
+
+    # convert trees to arrays to check whether they contain the same elements
+    ranKeys = []
+    dynKeys = []
+    ranPriorities = []
+    dynPriorities = []
+    ranLevels = []
+    dynLevels = []
+    ranDepth = treeDepth(randomTreap)
+    dynDepth = treeDepth(dynamicTreap)
+
+    treeArr(randomTreap, ranKeys, ranPriorities, ranLevels)
+    treeArr(dynamicTreap, dynKeys, dynPriorities, dynLevels)
+
+    # check if trees are the same and print depths of the trees
+    print "random Treap and dynamic Treap contain the same elements sorted the same way:", ranKeys == dynKeys, "\n"
+          
+    ranKeys = np.array(ranKeys)
+    dynKeys = np.array(dynKeys)
+    ranPriorities = np.array(ranPriorities)
+    dynPriorities = np.array(dynPriorities)
+    ranLevels = np.array(ranLevels)
+    dynLevels = np.array(dynLevels)
+
+    highest = np.max(dynPriorities) == dynPriorities
+
+    # print most frequent word and the number of its appearances
+    print "most frequent word:", dynKeys[highest][0], \
+          "\nnumber of apperances:", dynPriorities[highest][0], "\n"
+
+    # print depths of the trees (1d)
+    print "\ndepth of random Treap:", ranDepth, \
+          "\ndepth of dynamic Treap:", dynDepth, \
+          "\ndepth of balanced Treap:", np.log2(0.5*(treeSize(randomTreap)+1)), "\n"
+
+    # average access time (1d)
+    weightSum = 1.0*np.sum(dynPriorities)
+    print "average acces times", \
+          "\nrandom Treap:", 1.0*np.sum(dynPriorities*ranLevels)/weightSum, \
+          "\ndynamic Treap:", 1.0*np.sum(dynPriorities*dynLevels)/weightSum
