820d713fc726cdcef7e3ded21112eac2bcbc1022
Frederick Blumenthal <frederick.blumenthal@googlemail.com>
2012-06-08 02:04:09 +0200

    final version

--- Algos/06/bucketsort.py
+++ Algos/06/bucketsort.py
@@ -1,5 +1,7 @@
 import random
 import timeit
+import random
+import unittest
 from pylab import polyfit, poly1d
 import matplotlib.pyplot as plt
 from math import sqrt, fabs
@@ -74,64 +76,122 @@ def bucketSort(a, bucketMap, c):
         i += len(b[k])
 
              
-def func(x, m):
-    return m*x
+class SortingTestCase(unittest.TestCase):
+    """ Check post conditions for sorting algorithm
+
+    """
+
+    def setUp(self):
+        self.randomArray = [random.randint(0,1000) for r in xrange(100)]
+        self.sortedArray = self.randomArray[:]
+    
+    #check the size
+    def runTest(self):
+        self.assertEqual(len(self.randomArray), len(self.sortedArray), 
+                         'incorrect size')
+
+    #check that arrays contain same elements
+        _randomArray = self.randomArray[:]
+        for element in self.sortedArray:
+            index = 0
+            length = len(_randomArray)
+            while index < len(_randomArray):
+                if element == _randomArray[index]:
+                    _randomArray.pop(index)
+                    break
+                index += 1
+                self.assertEqual(len(_randomArray), length, 
+                                 'different elements in array')
+        
+    #check that array is sorted
+        last = self.sortedArray[0]
+        for element in self.sortedArray[1:]:
+            self.assertTrue(last <= element,'not sorted')
+            last = element
+
+#test bucketMapQuadratic
+class BucketSortTestCase1(SortingTestCase):
+    def setUp(self):
+        self.randomArray = [random.random() for r in xrange(1000)]
+        self.sortedArray = self.randomArray[:]
+        bucketSort(self.sortedArray, bucketMapQuadratic, 0.2)
+
+#test bucketMapLinear
+class BucketSortTestCase2(SortingTestCase):
+    def setUp(self):
+        self.randomArray = [random.random() for r in xrange(1000)]
+        self.sortedArray = self.randomArray[:]
+        bucketSort(self.sortedArray, bucketMapLinear, 0.2)
+
 
 if __name__ == "__main__":
         
+    print "takes about 2 minutes..."
 
     array_length = [10**i for i in xrange(2,7)]
     array_M = [2, 4, 8, 20, 100]
     n_total = len(array_M)*len(array_length)
 
     print "-"*40
-    print "run test for quadratic indexing:"
+    print "run test to check uniformity for quadratic indexing:"
     n_uniform_q = 0
-    #for l in array_length:
-    #    a = createData(l)
-    #    for M in array_M:
+    for l in array_length:
+        a = createData(l)
+        for M in array_M:
     #        print "Length = ", l, "  M = ",M 
-    #        buckets_quad = returnBuckets(a, bucketMapQuadratic, M)
-    #        tau = testUniformity(buckets_quad)
+            buckets_quad = returnBuckets(a, bucketMapQuadratic, M)
+            tau = testUniformity(buckets_quad)
     #        print 'tau = ', tau
-    #        if tau <= 3:
-    #            n_uniform_q += 1
-    #print "-"*40
-    #print "\n"
+            if tau <= 3:
+                n_uniform_q += 1
+    print "-"*40
+    print "\n"
 
-    #print "run test for linear indexing: "
-    #n_uniform_l = 0
-    #for l in array_length:
-    #    a = createData(l)
-    #    for M in array_M:
+    print "run test to check uniformity for linear indexing: "
+    n_uniform_l = 0
+    for l in array_length:
+        a = createData(l)
+        for M in array_M:
     #        print "Length = ", l, "  M = ",M 
-    #        buckets_lin = returnBuckets(a, bucketMapLinear, M)
-    #        tau = testUniformity(buckets_lin)
+            buckets_lin = returnBuckets(a, bucketMapLinear, M)
+            tau = testUniformity(buckets_lin)
     #        print 'tau = ', tau
-    #        if tau <= 3:
-    #            n_uniform_l += 1
+            if tau <= 3:
+                n_uniform_l += 1
 
-#    print "-"*79
-#    print "\n"
+    print "-"*79
+    print "\n"
 
-#    print "test result for quadratic indexing:"
-#    print n_uniform_q, " out of ", n_total, " tests passed"
-#    print "\n"
+    print "test result for quadratic indexing:"
+    print n_uniform_q, " out of ", n_total, " tests passed"
+    print "\n"
 
-#    print "test result for linear indexing:"
-#    print n_uniform_l, " out of ", n_total, " tests passed"
+    print "test result for linear indexing:"
+    print n_uniform_l, " out of ", n_total, " tests passed"
+    print "\n"
 
+    print "running unittest to check correctness of bucketSort\n"
 
+    ## testing bucketSort with both bucketMaps
+    sortingtestsuite = unittest.TestSuite()
+    sortingtestsuite.addTest(BucketSortTestCase1())
+    sortingtestsuite.addTest(BucketSortTestCase2())
     
-    n_length = [10**i for i in xrange(2,7)]
+    unittest.TextTestRunner(verbosity=2).run(sortingtestsuite)
+    
+
+    ## checking if runtime grows linear with arraysize
+    print "compute runtime..."
+
+
+    n_length = [1000, 5000, 10000, 20000, 40000, 100000, 200000, 
+                300000, 400000, 500000, 600000, 700000]
     time_quad = []
     time_lin = []
     
     for n in n_length:
-        #a = createData(n)
-        
         reps = 10
-        
+
         init="""                                                               
 import bucketsort as bs                                                  
 a = bs.createData(%d)
@@ -148,20 +208,15 @@ c=1.0/c
         time_quad.append(timer_quad.timeit(reps))               
         time_lin.append(timer_lin.timeit(reps))
         
-
+    # linear fit to data
     fit = polyfit(n_length, time_quad, 1)
-    m,b = polyfit(n_length, time_quad, 1)
     fit_fn = poly1d(fit)
     
     x=range(n_length[-1])
-    y=[]
-    for i in xrange(len(x)):
-        y.append(x[i]*m)
 
-
-    p_quad = plt.loglog(n_length, time_quad, 'bs')
-    p_lin  = plt.loglog(n_length, time_lin, 'yo')
-    p_fit  = plt.loglog(x, y, 'k')
+    p_quad = plt.plot(n_length, time_quad, 'bs')
+    p_lin  = plt.plot(n_length, time_lin, 'yo')
+    p_fit  = plt.plot(x, fit_fn(x), 'k')
 
     plt.legend([p_quad, p_lin, p_fit], ["int(r**2*M)", "int(r*M", "linear fit"], loc = 2)
 
@@ -172,21 +227,4 @@ c=1.0/c
     plt.axis([0, n_length[-1] + 10, 0, time_lin[-1] + 10])
     plt.show()
 
-
-
-
-#    a = createData(10)
-#    print a 
-#    insertionSort(a)
-#    print a
-
-#    a2 = createData(10)
-#    print a2
-#    bucketSort(a2, bucketMapQuadratic, 0.1)
-#    print a2
-    
-#    a3 = createData(10)
-#    print a3
-#    bucketSort(a3, bucketMapQuadratic, 0.1)
-#    print a3
 
