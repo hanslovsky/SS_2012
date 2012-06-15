3001b261a28301f28b96a7231cb985a9a52471d4
Philipp.Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-15 16:40:20 +0200

    7.2.2

---
+++ Pattern Recognition/07/ex07_files/errorFunction.R
@@ -0,0 +1,9 @@
+errorFunction=function(X, y, parameters) {
+  # X is a mx(p+1) matrix containing m samples of p random variables
+  # and ones. y is the corresponding mx1 set of datapoints. parameters
+  # is a (p+1)x1 vector containing the p+1 parameters of regression.
+
+  samples = dim(X)[1]
+  J = 1/(2*samples)*sum(X%*%parameters-y)
+  return J
+}
--- Pattern Recognition/07/ex07_files/ex07.R
+++ Pattern Recognition/07/ex07_files/ex07.R
@@ -7,14 +7,21 @@ pizzaData = read.csv(file="cities.csv",head=TRUE,sep=",")
 population = pizzaData$population
 profit = pizzaData$profit
 samples = length(population)
-X = rbind(population, rep(1, samples))
+X = cbind(population, rep(1, samples))
 y = profit
+maxPopulation = ceiling(max(X))
+minPopulation = floor(min(X))
+X
+y
 # X = population
 pdf(file="test.pdf")
-plot(X[1,],y,main='a fascinating plot',xlab='population',ylab='profit')
-dev.off()
 parameters = regression(X, y)
-#parameters
-#a = seq(0,20,.5)
-#b = parameters*a
-#plot(a,b,main='a fascinating plot',xlab='population',ylab='profit')
+parameters
+
+a = seq(minPopulation,maxPopulation,.5)
+b = parameters[1]*a + parameters[2]
+plot(a,b,main='pizza pizza',xlab='population',ylab='profit',type='l',col="#000000")
+points(X[,1],y, pch=6,cex=0.7,col='darkred')
+dev.off()
+
+
