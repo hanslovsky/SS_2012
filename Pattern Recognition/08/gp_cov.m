7ea82c38c5fb430e15f21c54bdb561619a4ecd6f
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-20 23:23:38 +0200

    corrected kernel

--- Pattern Recognition/08/gp_cov.m
+++ Pattern Recognition/08/gp_cov.m
@@ -1,10 +1,10 @@
 function covariance = gp_cov(x1, x2, theta0, theta1, theta2, theta3, beta)
 
 nugget = beta;
-dims = size(x1, 2);
-n1 = size(x1, 1);
-n2 = size(x2, 1);
-par1 = repmat(diag(x1*x1'),1,n2) + repmat(diag(x2*x2')',n1,1) - 2*x1*x2';
+dims = size(x1, 1);
+n1 = size(x1, 2);
+n2 = size(x2, 2);
+par1 = repmat(diag(x1'*x1),1,n2) + repmat(diag(x2'*x2)',n1,1) - 2*x1'*x2;
 delta = (par1 == 0);
 par1 = sqrt(par1);
 par3 = x1'*x2;
