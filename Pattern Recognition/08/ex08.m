ff456380dd31f56b26c72eae696777f3b74f74e7
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-20 23:00:19 +0200

    pattern gauss kernel

--- Pattern Recognition/08/ex08.m
+++ Pattern Recognition/08/ex08.m
@@ -1,7 +1,9 @@
 close all; clc; clear;
 
 x = (-1:0.1:1)';
+x1 = randsample(x, 10);
 [theta0, theta1, theta2, theta3, beta] = deal(9, 4, 0, 0, 0);
-covariance = gp_cov(x, x, theta0, theta1, theta2, theta3, beta)
-samples = exp(-x'*covariance\x);
-samples
+covariance = gp_cov(x, x, theta0, theta1, theta2, theta3, beta);
+
+
+sample = exp(-1/2*x1'*inv(covariance)*x1);
--- Pattern Recognition/08/gp_cov.m
+++ Pattern Recognition/08/gp_cov.m
@@ -1,8 +1,13 @@
 function covariance = gp_cov(x1, x2, theta0, theta1, theta2, theta3, beta)
-nugget = 0;
-if all(x1 == x2)
-    nugget = beta;
-par1 = (x1-x2)'*(x1-x2);
+
+nugget = beta;
+dims = size(x1, 2);
+n1 = size(x1, 1);
+n2 = size(x2, 1);
+par1 = repmat(diag(x1*x1'),1,n2) + repmat(diag(x2*x2')',n1,1) - 2*x1*x2';
+delta = (par1 == 0);
+par1 = sqrt(par1);
 par3 = x1'*x2;
-covariance = theta0*exp(-theta1/2*par1)+theta2+theta3*par3+nugget;
+covariance = theta0*exp(-theta1/2*par1)+theta2+theta3*par3+nugget*delta;
+
 end
