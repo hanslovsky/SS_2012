06ea7bb8d27605d4b12168b6ce6e2bfefb0529a8
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-06-22 10:12:31 +0200

    error to be corrected

--- Pattern Recognition/08/ex08.m
+++ Pattern Recognition/08/ex08.m
@@ -52,23 +52,34 @@ end
 hold off
 
 % 8.1.3
+% Gaussian Proccesses for Regressioen: A Quick Introduction [M. Ebden,
+% 2008]
+close all
 x = [-0.1 0.2, 0.8];
 y = [0.45 1.0 -1.0];
+xFaceColor = [0.1 0.5 1];
+uncertaintyFaceColor = [1 0.5 0.1];
 query = -2:0.05:2;
 [theta0, theta1, theta2, theta3, beta] = deal(1, 10, 0, 0, 0);
 sigma22 = gp_cov(x, x, theta0, theta1, theta2, theta3, beta);
 sigma21 = gp_cov(x, query, theta0, theta1, theta2, theta3, beta);
+sigma11 = gp_cov(query, query, theta0, theta1, theta2, theta3, beta);
 response = y*(sigma22\sigma21);
-uncertainty = sum(sigma21);
+uncertainty = diag(sigma11 - sigma21'*(sigma22\sigma21));
+uncertainty = sqrt(uncertainty');
 
 figure
 hold
-plot(x, y, 'd', 'MarkerEdgeColor', 'k', ...
-                'MarkerFaceColor', 'r', ...
-                'MarkerSize', 6)
-plot(query, response, 'k', 'LineWidth', 2)
-plot(query, response + uncertainty, 'g', 'Linewidth', 1)
-plot(query, response - uncertainty, 'g', 'Linewidth', 1)
+patch([query fliplr(query)], ...
+      [response+uncertainty,fliplr(response-uncertainty)], ...
+      uncertaintyFaceColor, 'EdgeColor', 'none')
+alpha(0.2)
+plot(query, response, 'k', 'LineWidth', 1)
+plot(query, response + uncertainty, 'Color', uncertaintyFaceColor - 0.1, 'Linewidth', 1)
+plot(query, response - uncertainty, 'Color', uncertaintyFaceColor - 0.1, 'Linewidth', 1)
+plot(x, y, '<', 'MarkerEdgeColor', 'none', ...
+                'MarkerFaceColor', xFaceColor, ...
+                'MarkerSize', 7)
 hold off
 
 
@@ -77,22 +88,29 @@ hold off
 
 
 % 8.1.4
-
+% Gaussian Proccesses for Regressioen: A Quick Introduction [M. Ebden,
+% 2008]
 
 [theta0, theta1, theta2, theta3, beta] = deal(1, 10, 0, 0, 20);
 sigma22 = gp_cov(x, x, theta0, theta1, theta2, theta3, beta);
 sigma21 = gp_cov(x, query, theta0, theta1, theta2, theta3, beta);
+sigma11 = gp_cov(query, query, theta0, theta1, theta2, theta3, beta);
 response = y*(sigma22\sigma21);
-uncertainty = sum(sigma21);
+uncertainty = diag(sigma11 - sigma21'*(sigma22\sigma21));
+uncertainty = sqrt(uncertainty');
 
 figure
 hold
-plot(x, y, 'd', 'MarkerEdgeColor', 'k', ...
-                'MarkerFaceColor', 'r', ...
-                'MarkerSize', 6)
-plot(query, response, 'k', 'LineWidth', 2)
-plot(query, response + uncertainty, 'g', 'Linewidth', 1)
-plot(query, response - uncertainty, 'g', 'Linewidth', 1)
+patch([query fliplr(query)], ...
+      [response+uncertainty,fliplr(response-uncertainty)], ...
+      uncertaintyFaceColor, 'EdgeColor', 'none')
+alpha(0.2)
+plot(query, response, 'k', 'LineWidth', 1)
+plot(query, response + uncertainty, 'Color', uncertaintyFaceColor - 0.1, 'Linewidth', 1)
+plot(query, response - uncertainty, 'Color', uncertaintyFaceColor - 0.1, 'Linewidth', 1)
+plot(x, y, '<', 'MarkerEdgeColor', 'none', ...
+                'MarkerFaceColor', xFaceColor, ...
+                'MarkerSize', 7)
 hold off
 
 
