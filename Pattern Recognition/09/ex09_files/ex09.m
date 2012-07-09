7a49b88c9993a0416ae341c413fafb9e68691313
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-07-09 02:01:04 +0200

    edited probability tables

--- Pattern Recognition/09/ex09_files/ex09.m
+++ Pattern Recognition/09/ex09_files/ex09.m
@@ -10,6 +10,7 @@ pts = n1*n2;
 img2 = [img_r, img_g, img_b];
 h = figure;
 scatter3(img2(:, 1), img2(:, 2), img2(:, 3), 0.1, [0.8 0.2 0.1], 'd')
+saveas(h, 'scatter.pdf', 'pdf');
 h = figure;
 [centers, post] = mip_kmeans(img2, K);
 img_compressed = reshape(centers(post, :), n1, n2, n3);
@@ -18,6 +19,7 @@ c2 = post == 2;
 c3 = post == 3;
 c4 = post == 4;
 scatter3(img2(:, 1), img2(:, 2), img2(:, 3), 0.1, post, 'd');
+saveas(h, 'centers.pdf', 'pdf');
 
 % 9.1.2
 h = figure;
@@ -41,6 +43,7 @@ end
 [X, Y] = ind2sub([n1, n2], 1:(n1*n2));
 img3 = [img2/255 X'/n1 Y'/n2];
 counter = 3;
+
 for K=[4 16 64]
     [centers, post] = mip_kmeans(img3, K);
     img_compressed = reshape(255*centers(post, 1:3), n1, n2, n3);
@@ -48,3 +51,28 @@ for K=[4 16 64]
     counter = counter + 3;
     imshow(uint8(round(img_compressed)))
 end
+saveas(h, 'compression.pdf', 'pdf');
+
+K = 4;
+h = figure;
+[centers, post] = mip_kmeans(img3, K);
+scatter3(img3(:, 1), img3(:, 2), img3(:, 3), 0.1, post, 'd');
+saveas(h, 'centersXY.pdf', 'pdf');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
