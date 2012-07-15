1c6f79ed4ce4c0c99ad14d875d2df66c8db2dc0d
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-07-15 02:08:33 +0200

    plots

--- Pattern Recognition/11/ex11_files/ex11.m
+++ Pattern Recognition/11/ex11_files/ex11.m
@@ -9,14 +9,22 @@ xmin = min(x);
 xmax = max(x);
 ymin = min(y);
 ymax = max(y);
+
+%l2 regression
 param = regression(X, y);
 xfit = [xmin-1000 xmax+1000];
 yfit = param(2) + param(1)*xfit; 
-% h = figure('Position', [50 800 1300 600]);
+h = figure;
 hold
 plot(x, y, 'd', 'MarkerSize', 4.6, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'k');
 axis([xmin-1000 xmax+1000 0 ymax+10])
 plot(xfit, yfit, 'k');
-hold off
-saveas(h, 'squared.pdf', 'pdf');
 
+% l1 regression
+A = [X -eye(m); -X -eye(m)];
+l1y = [y; -y];
+c = [zeros(p+1, 1); ones(m, 1)];
+l1par = linprog(c, A, l1y);
+l1y = l1par(2) + l1par(1)*xfit;
+plot(xfit, l1y);
+saveas(h, 'regression.pdf', 'pdf');
