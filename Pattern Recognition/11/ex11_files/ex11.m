92c688ce3d870c496eb30d20cde426b3b7feef55
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-07-15 02:26:53 +0200

    nur noch texen

--- Pattern Recognition/11/ex11_files/ex11.m
+++ Pattern Recognition/11/ex11_files/ex11.m
@@ -1,5 +1,6 @@
 close all; clear; clc
 
+linewidth = 1;
 cpu = csvread('cpu_load.csv', 1, 0);
 [m, p] = size(cpu(:, 1));
 x = cpu(:, 1);
@@ -16,9 +17,9 @@ xfit = [xmin-1000 xmax+1000];
 yfit = param(2) + param(1)*xfit; 
 h = figure;
 hold
-plot(x, y, 'd', 'MarkerSize', 4.6, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'k');
+plot(x, y, 'd', 'MarkerSize', 4.6, 'MarkerFaceColor', [0.6 0.6 0.6], 'MarkerEdgeColor', 'k');
 axis([xmin-1000 xmax+1000 0 ymax+10])
-plot(xfit, yfit, 'k');
+plot(xfit, yfit, 'Color', [0.8 0.3 0.3], 'Linewidth', linewidth);
 
 % l1 regression
 A = [X -eye(m); -X -eye(m)];
@@ -26,5 +27,9 @@ l1y = [y; -y];
 c = [zeros(p+1, 1); ones(m, 1)];
 l1par = linprog(c, A, l1y);
 l1y = l1par(2) + l1par(1)*xfit;
-plot(xfit, l1y);
+plot(xfit, l1y, 'Color', [0.5 0.7 0.2], 'LineWidth', linewidth);
+legend('data', 'L_2', 'L_1', 'Location', [0.75 0.75 0.1 0.1]);
+xlabel('BUFFER\_GETS');
+ylabel('CPU\_UTILIZE');
+box on
 saveas(h, 'regression.pdf', 'pdf');
